b511812ce84253c81b8d92e819db74396201d40f
Joo Park <andypark6004@gmail.com>
2022-10-28 14:13:14 -0400

    Adding simple functions
    Add
    Subtract
    Multiply
    Divide

---
+++ script.js
@@ -0,0 +1,16 @@
+function addC(a, b){
+    return a+b;
+}
+
+function subC(a, b){
+    return a-b;
+}
+
+
+function multiplyC(a, b){
+    return a*b;
+}
+
+function divideC(a, b){
+    return a/b ;
+}
