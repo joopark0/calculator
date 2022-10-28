d441068e03cf75a51395eaea46002aeb59706f72
Joo Park <andypark6004@gmail.com>
2022-10-28 14:33:01 -0400

    Adding calculator layout
    Added calculator body and screen to html
    Added some styling to see the screen itself

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ function multiplyC(a, b){
 function divideC(a, b){
     return a/b ;
 }
+
+function operate(operator, num1, num2){
+    switch(operator){
+        case '+':
+            return addC(num1, num2);
+        case '-':
+            return subC(num1, num2);
+        case '*':
+            return multiplyC(num1, num2);
+        case '/':
+            return divideC(num1, num2);
+    }
+}
