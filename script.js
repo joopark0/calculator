26af830e1b1bde2e239f8a8db8ada11b841e0424
Joo Park <andypark6004@gmail.com>
2022-10-30 00:03:41 -0400

    Adding effects
    Added hover effect to create radial gradient
    added borders to calculator

--- script.js
+++ script.js
@@ -135,3 +135,17 @@ const allButtons = document.querySelectorAll(".numBox");
 allButtons.forEach((button) => {
     button.addEventListener("click", inUpdate);
 })
+
+const handleOnMouseMove = e =>{
+    const { currentTarget: target} = e;
+
+    const rect = target.getBoundingClientRect(), x = e.clientX - rect.left, y = e.clientY - rect.top;
+
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+
+}
+
+for (const box of document.querySelectorAll(".numBox")){
+    box.onmousemove = e => handleOnMouseMove(e);
+}
